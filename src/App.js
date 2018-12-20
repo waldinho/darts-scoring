569f0c71edca4c6c59ecf5473ac5189eaa63365c
waldinho <james.waller1984@gmail.com>
2018-12-20 12:18:59 +1100

    styling and layout done

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ class App extends Component {
     this.addScore = this.addScore.bind(this);
     this.handleScoreChange1 = this.handleScoreChange1.bind(this);
     this.handleScoreChange2 = this.handleScoreChange2.bind(this);
+    this.handleNameChange1 = this.handleNameChange1.bind(this);
+    this.handleNameChange2 = this.handleNameChange2.bind(this);
     this.state = {
+      player1Name: '',
       player1scores: [],
       player1scoreInput: 0,
+      player2Name: '',
       player2scores: [],
       player2scoreInput: 0,
+      visible: true
     };
   }
 
@@ -27,6 +32,7 @@ class App extends Component {
       player2scores: newScores2,
       player1Sum: 501 - this.state.player1scores.reduce((a, b) => a + b, 0) - this.state.scoreInput1,
       player2Sum: 501 - this.state.player2scores.reduce((a, b) => a + b, 0) - this.state.scoreInput2,
+      visible: false,
     })
     if (player1Sum < this.state.scoreInput1) {
       this.setState({ 
@@ -66,16 +72,34 @@ class App extends Component {
     });
   }
 
+  handleNameChange1(e) {
+    this.setState({ 
+      player1Name: e.target.value,
+    });
+  }
+
+  handleNameChange2(e) {
+    this.setState({ 
+      player2Name: e.target.value,
+    });
+  }
+
   render() {
-    const player1Sum = this.state.player1Sum
-    const player2Sum = this.state.player2Sum
+    const { player1Sum, player2Sum, player1Name, player2Name, visible } = this.state
     return (
-      <div>
-        <input name="player1scores" type="number" max="180" placeholder="Score" onChange={this.handleScoreChange1}/>
-        <input name="player2scores" type="number" max="180" placeholder="Score" onChange={this.handleScoreChange2}/>
-        <button onClick={this.addScore.bind(this, this.state.scoreInput1, this.state.scoreInput2)}>add</button>
-        <p>{player1Sum ? player1Sum : '501'}</p>
-        <p>{player2Sum ? player2Sum : '501'}</p>
+      <div className='container'>
+        <div className='players'>
+          <p>{player1Name ? player1Name : 'Player 1'} - {player1Sum ? player1Sum : '501'}</p>
+          <p>{player2Name ? player2Name : 'Player 2'} - {player2Sum ? player2Sum : '501'}</p>
+        </div>
+        <div className='scorer'>
+          <input name="player1Name" type="text" placeholder="Player 1 name" onChange={this.handleNameChange1} hidden={ !visible }/>
+          <input name="player1scores" type="number" max="180" placeholder="Player 1 score" onChange={this.handleScoreChange1} />
+          <br/>
+          <input name="player2Name" type="text" placeholder="Player 2 name" onChange={this.handleNameChange2} hidden={ !visible }/>
+          <input name="player2scores" type="number" max="180" placeholder="Player 2 score" onChange={this.handleScoreChange2} />
+          <button onClick={this.addScore.bind(this, this.state.scoreInput1, this.state.scoreInput2)}>ADD</button>
+        </div>
       </div>
     );
   }
